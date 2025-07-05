bca70f89c1cd840af63afee740f8a21b5d2315fd
gmaheswaranmca <gmaheswaranmca@gmail.com>
2025-07-05 14:21:41 +0530

    au-guvi

---
+++ Codekata/Prgs/CoinChange.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class CoinChange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = sc.nextInt();
+        }
+        int amount = sc.nextInt();
+        int result = coinChange(coins, amount);
+        System.out.println(result);
+    }
+
+    public static int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
